4f2b8da32e282298b7ae4e3326c634cab68e07f7
j-sieg <siegmeister@protonmail.com>
2022-04-13 04:00:25 +0800

    Get Vite working with the default hotwire stuff

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,16 +1,7 @@
+// Hotwire
+import * as Turbo from '@hotwired/turbo'
+Turbo.start()
+import "./controllers"
+
+// Styles
 import "~/styles/application.css"
-console.log('Vite ⚡️ Rails')
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
