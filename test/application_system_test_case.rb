c20567084400c1f64f760c01c3c0d5b7d852ead6
James Siega <siegmeister@proton.me>
2022-08-14 09:22:38 +0800

    Make Calendar date switching possible with JavaScript

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,19 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  private
+
+  def login_as(user, password = "It's m3?")
+    visit login_url
+
+    within "form" do
+      fill_in "Email", with: user.email
+      fill_in "Password", with: password
+
+      click_on "Log in"
+    end
+
+    assert_text "Logged in successfully."
+  end
 end
---
+++ test/system/daily_expenses_test.rb
@@ -0,0 +1,23 @@
+require "application_system_test_case"
+
+class DailyExpensesTest < ApplicationSystemTestCase
+  test "date switching in #index" do
+    login_as(users(:james))
+
+    visit daily_expenses_url(date: "1999-11-27")
+
+    within "table[class='calendar--table']" do
+      assert_selector "td[class='today']", text: "27"
+      
+      # Switch to the 16th of November
+      find("td", text: "16").click
+      assert_selector "td[class='today']", text: "16"
+      assert_no_selector "td[class='today']", text: "27"
+
+      # Switch to the 24th of November
+      find("td", text: "24").click
+      assert_selector "td[class='today']", text: "24"
+      assert_no_selector "td[class='today']", text: "16"
+    end
+  end
+end
