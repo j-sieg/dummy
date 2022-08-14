d4104997b39a19602ea1e9a6aef2f0da0d6a72c1
James Siega <siegmeister@proton.me>
2022-08-14 09:39:32 +0800

    [MIGRATIONS] modify the create_daily_expenses expense -> amount add the purpose text field

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_222224) do
   create_table "daily_expenses", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.date "date", null: false
-    t.integer "expense", default: 0, null: false
+    t.integer "amount", default: 0
+    t.text "purpose"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_daily_expenses_on_user_id"
