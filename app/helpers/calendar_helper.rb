5efd4510e62e9e3e361d274c25a18394f7157f4a
James Siega <siegmeister@proton.me>
2022-08-15 11:08:26 +0800

    Mark dates on the calendar w/ expenses

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,21 +1,21 @@
 module CalendarHelper
-  def calendar(date: Date.current, path_method: :root_path, &block)
-    Calendar.new(self, date, path_method, block).table
+  def calendar(date: Date.current, path_method: :root_path, marked_dates: [])
+    Calendar.new(self, date, path_method, marked_dates).table
   end
 
   class Calendar
     HEADER = %w(Sun Mon Tue Wed Thu Fri Sat).freeze
     START_DAY = :sunday
 
-    attr_reader :view, :date, :block, :path_method
+    attr_reader :view, :date, :marked_dates, :path_method
 
     delegate :content_tag, to: :view
 
-    def initialize(view, date, path_method, block)
+    def initialize(view, date, path_method, marked_dates)
       @view = view
       @date = date
-      @block = block
       @path_method = path_method
+      @marked_dates = marked_dates
     end
 
     def table
@@ -65,11 +65,26 @@ module CalendarHelper
     end
 
     def day_cell(day)
-      content_tag :td, view.capture(day, &block), class: day_classes(day), data: {agenda_date: day.to_s}
+      content_tag :td, class: day_classes(day), data: {agenda_date: day.to_s} do
+        content_tag :div, class: "calendar--day" do
+          day_of_month = content_tag :div, class: "calendar--day-of-month" do
+            day.day.to_s
+          end
+
+          if marked_dates.include?(day)
+            marked_html = content_tag :div, class: "calendar--item" do
+              content_tag :span, "."
+            end
+            day_of_month + marked_html
+          else
+            day_of_month
+          end
+        end
+      end
     end
 
     def day_classes(day)
-      classes = []
+      classes = ["calendar--cell"]
       classes << "today" if day == date
       classes << "not--month" if day.month != date.month
       classes.empty? ? nil : classes.join(" ")
