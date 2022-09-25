646adf477cc366e18f5828694dc129a71bfbb2bd
James Siega <siegmeister@proton.me>
2022-09-26 00:21:49 +0800

    Fix the daily_expenses system test

--- test/system/daily_expenses_test.rb
+++ test/system/daily_expenses_test.rb
@@ -7,17 +7,17 @@ class DailyExpensesTest < ApplicationSystemTestCase
     visit daily_expenses_url(date: "1999-11-27")
 
     within "table[class='calendar--table']" do
-      assert_selector "td[class='today']", text: "27"
+      assert_selector "td[class='calendar--cell today']", text: "27"
       
       # Switch to the 16th of November
       find("td", text: "16").click
-      assert_selector "td[class='today']", text: "16"
-      assert_no_selector "td[class='today']", text: "27"
+      assert_selector "td[class='calendar--cell today']", text: "16"
+      assert_no_selector "td[class='calendar--cell today']", text: "27"
 
       # Switch to the 24th of November
       find("td", text: "24").click
-      assert_selector "td[class='today']", text: "24"
-      assert_no_selector "td[class='today']", text: "16"
+      assert_selector "td[class='calendar--cell today']", text: "24"
+      assert_no_selector "td[class='calendar--cell today']", text: "16"
     end
   end
 end
