37e3ee9ac9adfac389d8fbaecf5d24f66af5193f
James Siega <siegmeister@proton.me>
2022-10-31 23:10:55 +0800

    Generate the core models, migrations, and the registrations controller for now

---
+++ lib/generators/gen_auth/gen_auth_generator.rb
@@ -0,0 +1,28 @@
+class GenAuthGenerator < Rails::Generators::NamedBase
+  include Rails::Generators::Migration
+  source_root File.expand_path("templates", __dir__)
+
+  def self.next_migration_number(dirname)
+    ActiveRecord::Migration.new.next_migration_number(dirname.size + rand)
+  end
+
+  def create_controllers
+    template "controllers/registrations_controller.rb.erb", "app/controllers/#{plural_name}/registrations_controller.rb"
+  end
+
+  def create_models_migrations
+    migration_template "migrations/create_gen_auth_tables.rb.erb", "db/migrate/create_gen_auth_#{singular_name}_tables.rb"
+  end
+
+  # TODO:
+  # Password Model
+  # Views
+  # Routes
+  # Controllers
+  # Mailers
+
+  def create_models
+    template "models/model.rb.erb", "app/models/#{singular_name}.rb"
+    template "models/model_token.rb.erb", "app/models/#{singular_name}_token.rb"
+  end
+end
