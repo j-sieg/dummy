aedfca82fd909f52608e3c005bc8299966e734bc
James Siega <siegmeister@protonmail.com>
2024-02-06 21:03:01 +0800

    Create generators for the controllers and models and their tests

--- lib/generators/gen_auth/gen_auth_generator.rb
+++ lib/generators/gen_auth/gen_auth_generator.rb
@@ -7,25 +7,55 @@ class GenAuthGenerator < Rails::Generators::NamedBase
   end
 
   def create_controllers
-    template "controllers/application_controller.rb.erb", "app/controllers/#{plural_name}/application_controller.rb"
-    template "controllers/registrations_controller.rb.erb", "app/controllers/#{plural_name}/registrations_controller.rb"
-    template "controllers/sessions_controller.rb.erb", "app/controllers/#{plural_name}/sessions_controller.rb"
-    template "controllers/passwords_controller.rb.erb", "app/controllers/#{plural_name}/passwords_controller.rb"
+    controller_names = %w(
+      application_controller
+      confirmations_controller
+      passwords_controller
+      registrations_controller
+      reset_passwords_controller
+      sessions_controller
+      settings_controller
+    )
+
+    controller_names.each do |controller_name|
+      template "controllers/#{controller_name}.rb.erb", "app/controllers/#{plural_name}/#{controller_name}.rb"
+    end
+  end
+
+  def create_controller_tests
+    test_files = %w(
+      confirmations_controller_test
+      passwords_controller_test
+      registrations_controller_test
+      reset_passwords_controller_test
+      sessions_controller_test
+      settings_controller_test
+    )
+
+    test_files.each do |test_file|
+      template "tests/controllers/#{test_file}.rb.erb", "test/controllers/#{plural_name}/#{test_file}.rb"
+    end
   end
 
   def create_models_migrations
     migration_template "migrations/create_gen_auth_tables.rb.erb", "db/migrate/create_gen_auth_#{singular_name}_tables.rb"
   end
 
+  def create_views
+  end
+
   # TODO:
-  # Password Model
   # Views
   # Routes
-  # Controllers
   # Mailers
 
   def create_models
     template "models/model.rb.erb", "app/models/#{singular_name}.rb"
     template "models/model_token.rb.erb", "app/models/#{singular_name}_token.rb"
+  end
+
+  def create_model_tests
+    template "tests/models/model_test.rb.erb", "test/models/#{singular_name}_test.rb"
+    template "tests/models/model_token_test.rb.erb", "test/models/#{singular_name}_token_test.rb"
   end
 end
