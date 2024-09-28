fbb19b0aba4709b6f9a985a2d7bdac06481521f5
j-sieg <siegmeister@protonmail.com>
2024-09-29 06:35:46 +0800

    It might be the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,13 @@ def log(msg)
   puts "[ db/seeds.rb ] #{msg}"
 end
 
-unless Rails.env.development?
-  log "Skipping seeds. This is only supposed to run in the `development` environment"
-  exit 0
-end
-
-user = User.create!(email: "jpeck@example.com", password: "it's m3?", confirmed_at: Time.current)
-if user.persisted?
-  log "Created default User"
-  log "Email: jpeck@example.com"
-  log "Password: it's m3?"
+if Rails.env.development?
+  user = User.create!(email: "jpeck@example.com", password: "it's m3?", confirmed_at: Time.current)
+  if user.persisted?
+    log "Created default User"
+    log "Email: jpeck@example.com"
+    log "Password: it's m3?"
+  end
 end
 
 log "Done!"
